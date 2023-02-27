997fbd954214256c6c0fe26048cad1ab0af47caf
DebasishMohanta <debasish2018rta@gmail.com>
2023-02-27 21:47:00 +0530

    api and rest framework

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .serializers import MenuSerializer,BookingSerializer
+from .models import Menu,Booking
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
 
 def index(request):
     return render(request,'index1.html',{})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset= Menu.objects.all()
+    serializer_class = MenuSerializer 
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView,generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(APIView):
+    def get(self,request):
+        items = Booking.objects.all()
+        serialized_item = BookingSerializer(items,many=True)
+        return Response({"data": serialized_item.data})
+    def post(Self,request):
+        serializer = BookingSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success","data": serializer.data})
+
+
+
+
